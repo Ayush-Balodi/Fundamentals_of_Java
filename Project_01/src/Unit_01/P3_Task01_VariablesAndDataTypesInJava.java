f80a23ba4149d9c3d1adcbf5b9ddd62b82a42459
Ayush-Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2022-04-02 10:47:42 +0530

    Update font in Github

--- Project_01/src/Unit_01/P3_Task01_VariablesAndDataTypesInJava.java
+++ Project_01/src/Unit_01/P3_Task01_VariablesAndDataTypesInJava.java
@@ -28,13 +28,13 @@
  *  |----------------------------------------------|      
  *  | DATA TYPE                       DEFAULT SIZE |
  *  | byte                            1 byte       |
- *  | short							  2 bytes      | 
- *  | int 							  4 bytes      |
- *  | long							  8 bytes      |
- *  | float							  4 bytes      |
- *  | double						  8 bytes      |
- *  | boolean						  1 byte       |
- *  | char   						  2 bytes      |
+ *  | short			      2 bytes      | 
+ *  | int 			      4 bytes      |
+ *  | long			      8 bytes      |
+ *  | float			      4 bytes      |
+ *  | double			      8 bytes      |
+ *  | boolean			      1 byte       |
+ *  | char   			      2 bytes      |
  *  |----------------------------------------------|
  *  
  *  
