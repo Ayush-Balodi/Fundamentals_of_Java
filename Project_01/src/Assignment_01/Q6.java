46390eeae5729420d9adf6cbffcbfc550bf5955c
Ayush Balodi <ayushbalodidun@gmail.com>
2022-05-08 17:32:06 +0530

    Q6

---
+++ Project_01/src/Assignment_01/Q6.java
@@ -0,0 +1,52 @@
+/*
+ *	As given there are two sample methods: First one returns the sum of 
+ *  two numbers a and b and second one returns the subtraction of a and b. if 
+ *  a-b is negative then return value would be 0. Use ternary operator to call 
+ *  these two methods. If a>b then ternary should call sum(a,b) but if a<b 
+ *  then ternary operator should call sub(a,b). Eventually print the result of 
+ *  the ternary operation on variable a and b.
+ */
+package Assignment_01;
+
+import java.util.Scanner;
+
+public class Q6 {
+	public static void main( String[] args ) {
+		
+		Scanner obj = new Scanner( System.in );
+		
+		System.out.print("Enter the first number         : ");
+		int a = obj.nextInt();
+		
+		System.out.print("Enter the second number        : ");
+		int b = obj.nextInt();
+		
+		test obj1 = new test( a , b );
+		
+		int result = ( a > b ) ? obj1.first() : obj1.second();
+		
+		System.out.println("The result of the operation is : "+result);
+		obj.close();
+	}
+}
+
+class test{
+	int a , b;
+	
+	test(){
+		
+	}
+	
+	test( int a , int b ){
+		this.a = a;
+		this.b = b;
+	}
+	
+	int first() {
+		return ( a + b );
+	}
+	
+	int second() {
+		return ( ( a - b ) < 0 ) ? 0 : ( a - b );
+	}
+}
